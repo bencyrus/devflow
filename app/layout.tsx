fc85ed789c3b1307e9f788d3278ad32910d2990d
bencyrus <bencyrus@gmail.com>
2023-10-24 17:54:29 -0400

    added icon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+import './globals.css'
 
-export const metadata = {
-    title: 'Next.js 13 with Clerk',
+const inter = Inter({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
+    variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+    title: 'DevFlow',
+    description:
+        'A community driven platform for developers to share their knowledge and experiences, ask and answer questions, and learn new things.',
+    icons: { icon: '/favicon.ico' },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider>
+        <ClerkProvider
+            appearance={{
+                elements: {
+                    formButtonPrimary: 'primary-gradient',
+                    footerActionLink: 'primary-text-gradient hover:text-primary-500',
+                },
+            }}
+        >
             <html lang="en">
-                <body>{children}</body>
+                <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
             </html>
         </ClerkProvider>
     )
